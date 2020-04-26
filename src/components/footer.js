52c385c481cd9ea0752bb5f0fcd45305a0a08602
OCSP <58361304+ocsp@users.noreply.github.com>
2020-04-26 13:10:24 +0800

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,8 +5,8 @@ const Footer = () => {
   return (
     <div id="footer">
       <div className="container">
-        <p>基于 <Link to="https://uptimerobot.com/" text="UptimeRobot" /> 接口制作，检测频率 5 分钟</p>
-        <p>&copy; 2020 <Link to="https://status.org.cn/" text="STATUS.ORG.CN" /></p>
+        <p>Based on <Link to="https://uptimerobot.com/" text="UptimeRobot" /></p>
+        <p>&copy; 2020 <Link to="https://arknights.work" text="arknights.work" /></p>
       </div>
     </div>
   );
